5322f415ac83ee934f09420e9f757a4d9b7e3465
yanze <yan.zeltser@campus.technion.ac.il>
2022-12-19 15:54:56 +0200

    Fixed BRAM so it is inferred as a block in vivado.

--- src/common/Bram.sv
+++ src/common/Bram.sv
@@ -6,7 +6,6 @@
 * :param ADDR_BITS: Number of bits for the address field.
 * :param DATA_BITS: Number of bits for data field.
 * :input clk: System clock.
-* :input clear: Clears the RAM.
 * :input rd_addr: Address to read.
 * :input wr_en: Enable write to memory.
 * :input wr_addr: Address to write to.
@@ -19,7 +18,6 @@ module Bram #(
 )
 (
 	input logic clk,
-	input logic clear,
 	input logic [ADDR_BITS-1:0] rd_addr,
 	input logic wr_en,
 	input logic [ADDR_BITS-1:0] wr_addr,
@@ -30,19 +28,11 @@ module Bram #(
 	reg [DATA_BITS-1:0] mem [2**ADDR_BITS-1:0];
 
 	always_ff @(posedge clk) begin
-		if (clear) begin
-			integer i;
-			for (i = 0; i < 2**ADDR_BITS; i = i + 1) begin
-				mem[i] <= 0;
-			end
-			rd_data <= 0;
-		end
-		else begin
-			rd_data <= mem[rd_addr];
-			if (wr_en) begin
-				mem[wr_addr] <= wr_data;
-			end
+		if (wr_en) begin
+			mem[wr_addr] <= wr_data;
 		end
 	end // always_ff
+
+	assign rd_data = mem[rd_addr];
 
 endmodule : Bram
